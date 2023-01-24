1f03f974c05d3ec27641f87acfadbaec74d0fadc
WEE <yichengfeng77@gmail.com>
2023-01-23 20:54:02 -0500

    Upload Frontend Part

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Middleware section when API request hits
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
